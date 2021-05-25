1225529f214bccb45f5c4a5e0ae481558d6261f9
Weverton Diogo Procópio da Cruz <weverton@pop-os.localdomain>
2021-05-25 14:51:32 -0300

    draft of side-nav was created

---
+++ src/components/sidenav/sidenav.jsx
@@ -0,0 +1,15 @@
+import './sidenav.css'
+function SideNav() {
+    return(
+        <div className="side-nav">
+            <div className="container-side-nav">
+                <header className="header-side-nav">
+                    <div className="name">Bruno silas</div>
+                    <div className="photo"></div>
+                </header>
+            </div>
+        </div>
+    )
+}
+
+export default SideNav
