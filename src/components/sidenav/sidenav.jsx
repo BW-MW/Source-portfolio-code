477e343745ecc6517f4187171265d57262305a0b
Weverton Diogo Procópio da Cruz <weverton@pop-os.localdomain>
2021-05-25 14:55:03 -0300

    container were removed

--- src/components/sidenav/sidenav.jsx
+++ src/components/sidenav/sidenav.jsx
@@ -2,12 +2,10 @@ import './sidenav.css'
 function SideNav() {
     return(
         <div className="side-nav">
-            <div className="container-side-nav">
                 <header className="header-side-nav">
                     <div className="name">Bruno silas</div>
                     <div className="photo"></div>
                 </header>
-            </div>
         </div>
     )
 }
