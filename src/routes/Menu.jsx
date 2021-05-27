b2f11b75ddb475b06dd55b225faeeb44501b8245
Weverton Diogo Procópio da Cruz <weverton@pop-os.localdomain>
2021-05-27 15:22:35 -0300

    funçao temporaria de troca de market da rota atual

--- src/routes/Menu.jsx
+++ src/routes/Menu.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 function RenderLinks(props) {
   const { routers } = props;
+  const refs = useRef([]);
 
-  return <>{mapLinks(routers)}</>;
-}
+  useEffect(() => {
+    refs.current = refs.current?.slice(0, routers.length);
 
-function mapLinks(routers) {
-  return routers.map((route) => {
+    refs.current.map((ref) => {
+      if (ref.id === "Home") ref.parentNode.classList.add("active");
+
+      ref.addEventListener("click", (event) => {
+        const currentNav = event.target;
+        const currentLi = currentNav.parentNode;
+
+        for (let i in refs.current) {
+          const listNav = refs.current[i];
+
+          if (currentNav.textContent === listNav.textContent) {
+            currentLi.classList.add("active");
+          } else listNav.parentNode.classList.remove("active");
+        }
+      });
+    });
+  }, [routers]);
+
+  return routers.map((route, i) => {
     return (
       <li className="navbar-item" key={route.key}>
-        <Link className="navbar-link" to={route.path}>
+        <Link
+          className="navbar-link"
+          to={route.path}
+          id={route.label}
+          ref={(element) => (refs.current[i] = element)}
+        >
           {route.label}
         </Link>
       </li>
