c9fa1a06e948759dfbb88d42be8f16fb57f8571a
Weverton Diogo Procópio da Cruz <weverton@pop-os.localdomain>
2021-06-01 18:51:04 -0300

    draft was created

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,9 +1,20 @@
 import "./Portfolio.css";
 function Portfolio() {
   return (
-    <header className="header-portfolio">
-      <h1 className="title">Portifólio</h1>
-    </header>
+    <>
+      <header className="header-portfolio">
+        <h1 className="title">Portifólio</h1>
+      </header>
+      <section className="main-content">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </section>
+    </>
   );
 }
+
 export default Portfolio;
