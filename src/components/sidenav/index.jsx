d03ad32cc210cbfff92791e4e56432cc1b277627
Weverton Diogo Procópio da Cruz <weverton@pop-os.localdomain>
2021-06-04 09:59:22 -0300

    layout change based on user screen

--- src/components/sidenav/index.jsx
+++ src/components/sidenav/index.jsx
@@ -3,12 +3,26 @@ import "./styles.css";
 import Desktop from "./desktop";
 
 import Mobile from "./mobile";
+import { useLayoutEffect, useState } from "react";
 function SideNav() {
-  return checking();
+  const width = useWindowSize();
+  return checking(width);
 }
 
-function checking() {
-  if (window.innerWidth >= 992) {
+function useWindowSize() {
+  const [size, setSize] = useState(0);
+
+  useLayoutEffect(() => {
+    const updateSize = () => setSize(window.innerWidth);
+    window.addEventListener("resize", updateSize);
+    updateSize();
+  });
+
+  return size;
+}
+
+function checking(width) {
+  if (width >= 992) {
     return <Desktop />;
   } else {
     return <Mobile />;
