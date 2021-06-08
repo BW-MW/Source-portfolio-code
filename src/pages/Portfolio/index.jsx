c50430adab342c2a54895fe0a79814ab0888bf0f
Bruno Silas dos Santos <santosb@pop-os.localdomain>
2021-06-07 23:59:16 -0300

    new changes certainly happend here again

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -2,7 +2,8 @@ import "./styles.css";
 function Portfolio() {
   return (
     <>
-        <h1 className="title">Portifólio</h1>
+      <h1 className="title">Portifólio</h1>
+
       <section className="main-content">
         <div className="box"></div>
         <div className="box"></div>
